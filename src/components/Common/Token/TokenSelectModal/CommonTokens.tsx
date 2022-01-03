c843db92ab6947f5c4618ae056c66a14ee968872
Robb Veltman <robb.veltman@gmail.com>
2022-01-03 13:40:37 -0500

    Clean up CSS and create CommonTokens subcomponent

---
+++ src/components/Common/Token/TokenSelectModal/CommonTokens.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+import { commonBaseTokenSymbols, getToken } from "config/networks";
+import { useConnectedWeb3Context } from "contexts";
+import { IToken } from "types";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    flexWrap: "wrap",
+    margin: "0 -4px",
+  },
+  token: {
+    border: `1px solid ${theme.colors.primary200}`,
+    display: "flex",
+    alignItems: "center",
+    padding: 4,
+    paddingRight: 12,
+    borderRadius: 17,
+    margin: 4,
+    cursor: "pointer",
+    transition: "all 0.4s",
+    "&:hover": { opacity: 0.7 },
+    "& img": {
+      width: 24,
+      height: 24,
+      borderRadius: "50%",
+      marginRight: 8,
+    },
+    "& span": {
+      color: theme.colors.white,
+      fontSize: 14,
+      fontWeight: 700,
+    },
+  },
+}));
+
+interface IProps {
+  onSelectToken: (_: IToken) => void;
+}
+
+export const CommonTokens: React.FC<IProps> = ({ onSelectToken }) => {
+  const cl = useStyles();
+  const { networkId } = useConnectedWeb3Context();
+
+  return (
+    <div className={cl.root}>
+      {commonBaseTokenSymbols.map((tokenId) => {
+        const token = getToken(tokenId as any, networkId);
+        return (
+          <span
+            className={cl.token}
+            key={token.address}
+            onClick={() => onSelectToken(token)}
+          >
+            <img alt="img" src={token.image || ""} />
+            <span>{token.symbol}</span>
+          </span>
+        );
+      })}
+    </div>
+  )
+}
--- src/components/Common/Token/TokenSelectModal/index.tsx
+++ src/components/Common/Token/TokenSelectModal/index.tsx
@@ -21,71 +21,57 @@ import { useConnectedWeb3Context } from "contexts";
 import { transparentize } from "polished";
 import React from "react";
 import { IToken } from "types";
+import { CommonTokens } from "./CommonTokens";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    position: "absolute",
-    width: 600,
-    backgroundColor: theme.colors.primary500,
-
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    outline: "none",
-    maxHeight: "80vh",
-    userSelect: "none",
-    overflowY: "auto",
-    borderRadius: 4,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    width: "100vw",
+    height: "100vh",
   },
-  content: { position: "relative", padding: 24 },
-  tokenList: { padding: 24, backgroundColor: theme.colors.seventh },
+  content: {
+    position: "relative",
+    outline: "none",
+    width: 700,
+    maxWidth: "90vw",
+    maxHeight: "80vh",
+    backgroundColor: theme.colors.primary500,
+  },
+  topSection: {
+    padding: theme.spacing(3),
+  },
+  tokenList: {
+    backgroundColor: theme.colors.seventh,
+    padding: theme.spacing(3),
+    width: "100%",
+  },
   title: {
     fontSize: 20,
     color: theme.colors.white,
-    marginBottom: 16,
+    marginBottom: theme.spacing(2),
   },
-  close: { position: "absolute", padding: 12, top: 0, right: 0 },
-  searchIcon: { color: theme.colors.eighth },
+  close: {
+    position: "absolute",
+    padding: 12,
+    top: 4,
+    right: 4,
+  },
+  searchIcon: {
+    color: theme.colors.eighth
+  },
   search: {
-    border: `1px solid ${theme.colors.purple0} `,
+    border: `1px solid ${theme.colors.purple0}`,
     borderRadius: 4,
-    padding: "8px 12px ",
+    padding: theme.spacing(1),
   },
   commonLabel: {
     color: theme.colors.primary100,
     fontWeight: 700,
     fontSize: 10,
-    marginTop: 24,
-    marginBottom: 8,
-  },
-  commonTokens: {
-    display: "flex",
-    alignItems: "center",
-    flexWrap: "wrap",
-    margin: "0 -4px",
-  },
-  commonToken: {
-    border: `1px solid ${theme.colors.primary200}`,
-    padding: 4,
-    paddingRight: 12,
-    display: "flex",
-    alignItems: "center",
-    borderRadius: 17,
-    margin: 4,
-    cursor: "pointer",
-    transition: "all 0.4s",
-    "&:hover": { opacity: 0.7 },
-    "& img": {
-      width: 24,
-      height: 24,
-      borderRadius: "50%",
-      marginRight: 8,
-    },
-    "& span": {
-      color: theme.colors.white,
-      fontSize: 14,
-      fontWeight: 700,
-    },
+    margin: theme.spacing(3, 0, 1, 0),
   },
   token: {
     display: "flex",
@@ -134,63 +120,51 @@ export const TokenSelectModal: React.FC<IProps> = ({
     <Modal open={open} onClose={onClose}>
       <div className={clsx(classes.root, className)}>
         <div className={classes.content}>
-          <Typography className={classes.title}>Select Token</Typography>
-          <IconButton className={classes.close} onClick={onClose}>
-            <img alt="close" src="/assets/icons/close.svg" />
-          </IconButton>
-          <div>
-            <TextField
-              className={classes.search}
-              fullWidth
-              InputProps={{
-                disableUnderline: true,
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <SearchIcon className={classes.searchIcon} />
-                  </InputAdornment>
-                ),
-              }}
-              variant="standard"
-              color="primary"
-              placeholder="Search by token name or paste address"
-            />
-            <Typography className={classes.commonLabel}>
-              COMMON BASES
-            </Typography>
-            <div className={classes.commonTokens}>
-              {commonBaseTokenSymbols.map((tokenId) => {
-                const token = getToken(tokenId as any, networkId);
-                return (
-                  <span
-                    className={classes.commonToken}
-                    key={token.address}
-                    onClick={() => onSelect(token)}
-                  >
-                    <img alt="img" src={token.image || ""} />
-                    <span>{token.symbol}</span>
-                  </span>
-                );
-              })}
+          <div className={classes.topSection}>
+            <Typography className={classes.title}>Select Token</Typography>
+            <IconButton className={classes.close} onClick={onClose}>
+              <img alt="close" src="/assets/icons/close.svg" />
+            </IconButton>
+            <div>
+              <TextField
+                className={classes.search}
+                fullWidth
+                InputProps={{
+                  disableUnderline: true,
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SearchIcon className={classes.searchIcon} />
+                    </InputAdornment>
+                  ),
+                }}
+                variant="standard"
+                color="primary"
+                placeholder="Search by token name or paste address"
+              />
+              <Typography className={classes.commonLabel}>
+                COMMON BASES
+              </Typography>
+              <CommonTokens onSelectToken={onSelect} />
             </div>
           </div>
-        </div>
-        <div className={classes.tokenList}>
-          {tokenSymbols.map((tokenId) => {
-            const token = getToken(tokenId as any, networkId);
-            return (
-              <div
-                className={classes.token}
-                key={token.address}
-                onClick={() => onSelect(token)}
-              >
-                <img alt="img" src={token.image || ""} />
-                <div>
-                  <p>{token.symbol}</p>
-                  <span>{token.name}</span>
+          <div className={classes.tokenList}>
+            {tokenSymbols.map((tokenId) => {
+              const token = getToken(tokenId as any, networkId);
+              return (
+                <div
+                  className={classes.token}
+                  key={token.address}
+                  onClick={() => onSelect(token)}
+                >
+                  <img alt="img" src={token.image || ""} />
+                  <div>
+                    <p>{token.symbol}</p>
+                    <span>{token.name}</span>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
     </Modal>
