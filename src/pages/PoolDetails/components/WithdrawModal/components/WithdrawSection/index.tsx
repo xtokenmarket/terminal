e5207c3231c1757c580cfc5622311dd6690a099e
Nora Liu <noraliu0830@gmail.com>
2022-02-09 21:12:27 +0800

    feat: add withdraw / withdrawAndClaimReward

--- src/pages/PoolDetails/components/WithdrawModal/components/WithdrawSection/index.tsx
+++ src/pages/PoolDetails/components/WithdrawModal/components/WithdrawSection/index.tsx
@@ -100,26 +100,27 @@ export const WithdrawSection = (props: IProps) => {
         withdrawing: true,
       }))
 
-      const txId = await lmService[
-        withdrawState.withdrawOnly
-          ? 'removeLiquidity'
-          : 'removeLiquidityAndClaimReward'
-      ](poolData.address, withdrawState.lpInput)
-      const finalTxId = await lmService.waitUntilRemoveLiquidity(
+      const clr = new CLRService(provider, account, poolData.address)
+
+      const txId = await clr[
+        withdrawState.withdrawOnly ? 'withdraw' : 'withdrawAndClaimReward'
+      ](withdrawState.lpInput)
+
+      const finalTxId = await clr.waitUntilWithdraw(
         poolData.address,
         withdrawState.lpInput,
         account,
         txId
       )
 
-      const data = await lmService.parseRemoveLiquidityTx(finalTxId)
-      const clr = new CLRService(provider, account, poolData.address)
+      const data = await clr.parseWithdrawTx(finalTxId)
+
       const totalLiquidity = await clr.getTotalLiquidity()
 
       const claimedEarn: BigNumber[] = []
 
       if (!withdrawState.withdrawOnly) {
-        const claimInfo = await lmService.parseClaimTx(finalTxId)
+        const claimInfo = await clr.parseClaimTx(finalTxId)
         poolData.rewardState.tokens.forEach((rewardToken) => {
           const rewardAmount =
             claimInfo[rewardToken.address.toLowerCase()] || ZERO
--- src/services/clr.ts
+++ src/services/clr.ts
@@ -42,12 +42,6 @@ class CLRService {
     return this.contract.getTotalLiquidity()
   }
 
-  withdraw = async (
-    amount: BigNumber
-  ) => {
-    return this.contract.withdraw(amount)
-  }
-
   deposit = async (
     inputAsset: number,
     amount: BigNumber
@@ -121,6 +115,114 @@ class CLRService {
       }
     }
     return null
+  }
+
+  withdrawAndClaimReward = async (
+    amount: BigNumber
+  ): Promise<string> => {
+    
+    
+    const transactionObject = await this.contract.withdrawAndClaimReward(
+      amount
+    )
+    console.log(`withdrawAndClaimReward transaction hash: ${transactionObject.hash}`)
+
+    return transactionObject.hash
+  }
+
+  withdraw = async (
+    amount: BigNumber
+  ): Promise<string> => {
+    const transactionObject = await this.contract.withdraw(
+      amount
+    )
+    console.log(`withdraw transaction hash: ${transactionObject.hash}`)
+
+    return transactionObject.hash
+  }
+
+  parseWithdrawTx = async (
+    txId: string
+  ): Promise<{
+    amount0: BigNumber
+    amount1: BigNumber
+    liquidity: BigNumber
+  } | null> => {
+    const { logs } = await this.contract.provider.getTransactionReceipt(txId)
+    const uniPositionInterface = new Interface(Abi.UniswapV3Position)
+    for (let index = 0; index < logs.length; index++) {
+      const log = logs[index]
+      try {
+        const parsed = uniPositionInterface.parseLog(log)
+        if (parsed.name === 'DecreaseLiquidity') {
+          return {
+            amount0: parsed.args[2],
+            amount1: parsed.args[3],
+            liquidity: parsed.args[1],
+          }
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    return null
+  }
+
+  parseClaimTx = async (
+    txId: string
+  ): Promise<{
+    [key: string]: BigNumber
+  }> => {
+    const result: {
+      [key: string]: BigNumber
+    } = {}
+    const { logs } = await this.contract.provider.getTransactionReceipt(txId)
+    const uniPositionInterface = new Interface(Abi.xAssetCLR)
+    for (let index = 0; index < logs.length; index++) {
+      const log = logs[index]
+      try {
+        const parsed = uniPositionInterface.parseLog(log)
+        if (parsed.name === 'RewardClaimed') {
+          result[String(parsed.args[1]).toLowerCase()] = parsed.args[2]
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    return result
+  }
+
+  waitUntilWithdraw = async (
+    clrPool: string,
+    amount: BigNumber,
+    account: string,
+    txId: string
+  ): Promise<string> => {
+    let resolved = false
+    return new Promise((resolve) => {
+      this.contract.on(
+        'Withdraw',
+        (clrPoolAddress: string, sender: string, amountStr: any, ...rest) => {
+          if (
+            clrPool.toLowerCase() === clrPoolAddress.toLowerCase() &&
+            account.toLowerCase() === sender.toLowerCase() &&
+            amount.eq(BigNumber.from(amountStr))
+          ) {
+            if (!resolved) {
+              resolved = true
+              resolve(rest[0].transactionHash)
+            }
+          }
+        }
+      )
+
+      this.contract.provider.waitForTransaction(txId).then(() => {
+        if (!resolved) {
+          resolved = true
+          resolve(txId)
+        }
+      })
+    })
   }
 }
 
