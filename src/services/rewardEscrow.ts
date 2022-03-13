9134bf3263c17878ca94f35714918049dbd6f436
Eswara Sai <eswarasai.v@gmail.com>
2022-03-14 00:31:09 +0530

    Added all the vesting entries

--- src/services/rewardEscrow.ts
+++ src/services/rewardEscrow.ts
@@ -31,6 +31,25 @@ class RewardEscrowService {
     return this.contract.clrPoolVestingPeriod(addr)
   }
 
+  checkAccountSchedule = async (
+    poolAddress: string,
+    tokenAddress: string,
+    account: string
+  ) => {
+    const entries = await this.contract.checkAccountSchedule(
+      poolAddress,
+      tokenAddress,
+      account
+    )
+    const vestingEntries = entries.filter((s: BigNumber) => !s.isZero())
+    const amounts: BigNumber[] = []
+    const timestamps: BigNumber[] = []
+    vestingEntries.map((entry: BigNumber, index: number) =>
+      index % 2 === 0 ? timestamps.push(entry) : amounts.push(entry)
+    )
+    return { amounts, timestamps }
+  }
+
   getNextVestingEntry = async (
     address: string,
     tokenAddress: string,
@@ -39,26 +58,20 @@ class RewardEscrowService {
     const [timestamp, amount] = await this.contract.getNextVestingEntry(
       address,
       tokenAddress,
-      account,
+      account
     )
 
     return { timestamp, amount }
   }
 
-  vestAll = async (
-    poolAddress: string,
-    tokenAddresses: string[],
-  ) => {
+  vestAll = async (poolAddress: string, tokenAddresses: string[]) => {
     const txObj = await this.contract.vestAll(poolAddress, tokenAddresses)
     return txObj.hash
   }
 
-  waitUntilVestAll = async (
-    account: string,
-    txId: string,
-  ): Promise<string> => {
+  waitUntilVestAll = async (account: string, txId: string): Promise<string> => {
     let resolved = false
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       this.contract.on('Vested', (clrPool: string, sender: any, ...rest) => {
         if (account.toLowerCase() === sender.toLowerCase()) {
           if (!resolved) {
@@ -98,20 +111,28 @@ class RewardEscrowService {
   getVestedBalance = async (
     address: string,
     tokenAddress: string,
-    account: string,
+    account: string
   ): Promise<any> => {
-    let sum = BigNumber.from(0);
+    let sum = BigNumber.from(0)
     try {
-      const latestBlock = await this.provider.getBlock("latest");
-      const numVestingEntries = await this.contract.numVestingEntries(address, tokenAddress, account);
-      const schedule = await this.contract.checkAccountSchedule(address, tokenAddress, account);
+      const latestBlock = await this.provider.getBlock('latest')
+      const numVestingEntries = await this.contract.numVestingEntries(
+        address,
+        tokenAddress,
+        account
+      )
+      const schedule = await this.contract.checkAccountSchedule(
+        address,
+        tokenAddress,
+        account
+      )
       if (numVestingEntries > 0) {
         for (let i = 0; i < +numVestingEntries * 2; i = i + 2) {
-          const t = schedule[i];
-          const v = schedule[i + 1];
+          const t = schedule[i]
+          const v = schedule[i + 1]
           if (t.gt(0) && v.gt(0)) {
             if (t.lt(latestBlock.timestamp)) {
-              sum = BigNumber.from(sum).add(v);
+              sum = BigNumber.from(sum).add(v)
             }
           }
         }
@@ -119,7 +140,7 @@ class RewardEscrowService {
     } catch (error) {
       console.error(error)
     }
-    return sum;
+    return sum
   }
 }
 
