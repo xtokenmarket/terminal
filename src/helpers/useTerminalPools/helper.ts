d0c5d94c56456b423e295c5f23c4f075eb3a0fda
Eswara Sai <eswarasai.v@gmail.com>
2022-04-11 16:41:21 +0530

    Handled fetching discover pools from subgraph

---
+++ src/helpers/useTerminalPools/helper.ts
@@ -0,0 +1,111 @@
+import { Network } from 'utils/enums'
+import { AddressZero } from '@ethersproject/constants'
+import { getAddress } from 'ethers/lib/utils'
+
+export const GRAPHQL_URLS = [
+  'https://api.thegraph.com/subgraphs/name/miracle-arrow/terminal-mainnet',
+  'https://api.thegraph.com/subgraphs/name/miracle-arrow/terminal-arbitrum',
+  'https://api.thegraph.com/subgraphs/name/miracle-arrow/terminal-optimism',
+  'https://api.thegraph.com/subgraphs/name/miracle-arrow/terminal-polygon',
+  'https://api.thegraph.com/subgraphs/name/miracle-arrow/terminal-kovan',
+]
+
+export const POOLS_QUERY = `
+query {
+  pools(first: 1000) {
+      id
+      token0 {
+        id
+        symbol
+        name
+        decimals
+      }
+      token1 {
+        id
+        symbol
+        name
+        decimals
+      }
+      ticks
+      owner {
+        id
+      }
+      manager {
+        id
+      }
+      rewardTokens {
+        id
+        symbol
+        name
+        decimals
+      }
+      rewardAmounts
+      rewardAmountsPerWeek
+      rewardsAreEscrowed
+      rewardDuration
+      tokenId
+      poolFee
+      tradeFee
+      uniswapPool
+      periodFinish
+      stakedToken {
+        id
+        symbol
+        name
+        decimals
+      }
+      vestingPeriod
+      bufferTokenBalance
+      stakedTokenBalance
+    }
+}
+`
+
+export const NETWORKS = [
+  Network.MAINNET,
+  Network.ARBITRUM,
+  Network.OPTIMISM,
+  Network.POLYGON,
+  Network.KOVAN,
+]
+
+export const parsePools = (data: any, network: Network) => {
+  return data.errors
+    ? []
+    : data.pools.map((pool: any) => ({
+        poolAddress: getAddress(pool.id),
+        network,
+        manager: pool.manager ? getAddress(pool.manager.id) : AddressZero,
+        owner: pool.owner ? getAddress(pool.owner.id) : AddressZero,
+        periodFinish: pool.periodFinish || '0',
+        poolFee: pool.poolFee.toString(),
+        rewardProgramDuration: pool.rewardDuration,
+        rewardTokens: pool.rewardTokens.map((token: any) =>
+          _parseTokenDetails(token)
+        ),
+        rewardsAreEscrowed: pool.rewardsAreEscrowed,
+        stakedToken: _parseTokenDetails(pool.stakedToken),
+        token0: _parseTokenDetails(pool.token0),
+        token1: _parseTokenDetails(pool.token1),
+        tokenId: pool.tokenId,
+        ticks: {
+          tick0: pool.ticks[0],
+          tick1: pool.ticks[1],
+        },
+        totalRewardAmounts: pool.rewardAmounts || [],
+        tradeFee: pool.tradeFee,
+        uniswapPool: pool.uniswapPool
+          ? getAddress(pool.uniswapPool.id)
+          : AddressZero,
+        vestingPeriod: pool.vestingPeriod || '0',
+      }))
+}
+
+const _parseTokenDetails = (token: any) => {
+  return {
+    address: token.id,
+    decimals: token.decimals,
+    name: token.name,
+    symbol: token.symbol,
+  }
+}
--- src/helpers/useTerminalPools/index.tsx
+++ src/helpers/useTerminalPools/index.tsx
@@ -3,14 +3,17 @@ import { TERMINAL_API_URL } from 'config/constants'
 import { useNetworkContext } from 'contexts/networkContext'
 import { useEffect, useState } from 'react'
 import { ITerminalPool } from 'types'
-import { waitSeconds } from 'utils'
 import { Network } from 'utils/enums'
 import { isTestnet } from 'utils/network'
+import { fetchQuery } from 'utils/thegraph'
+import { GRAPHQL_URLS, NETWORKS, parsePools, POOLS_QUERY } from './helper'
 
 interface IState {
   isLoading: boolean
   pools: ITerminalPool[]
 }
+
+const testNetworks = [Network.KOVAN, Network.RINKEBY]
 
 export const useTerminalPools = () => {
   const [state, setState] = useState<IState>({ pools: [], isLoading: true })
@@ -18,12 +21,11 @@ export const useTerminalPools = () => {
 
   const loadPools = async () => {
     setState((prev) => ({ ...prev, isLoading: true }))
+
     try {
-      await waitSeconds(1)
       const { data: pools } = await axios.get<ITerminalPool[]>(
         `${TERMINAL_API_URL}/pools`
       )
-      const testNetworks = [Network.KOVAN, Network.RINKEBY]
       const filteredPools = isTestnet(chainId)
         ? pools.filter((pool) => testNetworks.includes(pool.network as Network))
         : pools.filter(
@@ -35,7 +37,28 @@ export const useTerminalPools = () => {
         isLoading: false,
       }))
     } catch (error) {
-      setState((prev) => ({ ...prev, isLoading: false }))
+      try {
+        let pools = await Promise.all(
+          GRAPHQL_URLS.map((url: string) => fetchQuery(POOLS_QUERY, {}, url))
+        )
+        pools = pools
+          .map((p: any, index: number) => parsePools(p, NETWORKS[index]))
+          .flat()
+        const filteredPools = isTestnet(chainId)
+          ? pools.filter((pool) =>
+              testNetworks.includes(pool.network as Network)
+            )
+          : pools.filter(
+              (pool) => !testNetworks.includes(pool.network as Network)
+            )
+        setState((prev) => ({
+          ...prev,
+          pools: filteredPools,
+          isLoading: false,
+        }))
+      } catch (e) {
+        setState((prev) => ({ ...prev, isLoading: false }))
+      }
     }
   }
 
