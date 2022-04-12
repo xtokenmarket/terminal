a38833f2f216760dd52d21e84b68fe984896d9d9
conache <C.conache@gmail.com>
2022-04-12 18:23:26 +0300

    Added proper styling for reusable components in vesting step

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -110,6 +110,13 @@ export enum Network {
   RINKEBY = 'rinkeby',
 }
 
+export enum EPeriods {
+  Days = 'Days',
+  Weeks = 'Weeks',
+  Months = 'Months',
+  Years = 'Years',
+}
+
 // uni v3
 export enum PoolState {
   LOADING,
@@ -141,7 +148,7 @@ export enum ECreareTokenSaleStep {
   Offering = 'Offering',
   Auction = 'Auction',
   Vesting = 'Vesting',
-  Confirm = 'Confirm'
+  Confirm = 'Confirm',
 }
 
 export enum IpricingFormula {
@@ -149,4 +156,3 @@ export enum IpricingFormula {
   Ascending = 'Ascending',
   Descending = 'Descending',
 }
-
