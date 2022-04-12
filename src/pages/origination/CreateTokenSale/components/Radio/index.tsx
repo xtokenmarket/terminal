224b56e3d1820133c60c06bd8c912e5e23e4217d
conache <C.conache@gmail.com>
2022-04-12 15:46:16 +0300

    Radio item style added

--- src/pages/origination/CreateTokenSale/components/Radio/index.tsx
+++ src/pages/origination/CreateTokenSale/components/Radio/index.tsx
@@ -8,6 +8,18 @@ const useStyles = makeStyles((theme) => ({
   item: {
     display: 'flex',
     alignItems: 'center',
+    '&:not(:last-of-type)': {
+      marginBottom: theme.spacing(2),
+    },
+  },
+  checkboxIcon: {
+    position: 'absolute',
+    left: 0,
+    width: 16,
+    height: 16,
+  },
+  checkboxRoot: {
+    marginRight: 8,
   },
 }))
 
@@ -29,11 +41,23 @@ export const Radio: React.FC<IProps> = ({ items, onChange, className }) => {
       {items.map((item: string) => (
         <div key={item} className={classes.item}>
           <Checkbox
-            style={{
-              color: '#6E27E4',
-            }}
             checked={value === item}
             color="secondary"
+            classes={{
+              root: classes.checkboxRoot,
+            }}
+            icon={
+              <img
+                src="/assets/icons/unchecked.svg"
+                className={classes.checkboxIcon}
+              />
+            }
+            checkedIcon={
+              <img
+                src="/assets/icons/checked-white.svg"
+                className={classes.checkboxIcon}
+              />
+            }
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
               onChange(e)
               setValue(item)
