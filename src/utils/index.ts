dd7b3ac046140f9109e63e69bff527340dfce4d0
Nora Liu <noraliu0830@gmail.com>
2022-04-18 11:54:03 -0400

    feat: match params types on CreateTokenSale

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -212,3 +212,22 @@ export const formatDateTime = (timestamp: number) => {
     day: 'numeric',
   }).format(new Date(timestamp * 1000))
 }
+
+export const getDurationSecStr = (amount: number, unit: string) => {
+  let durationSec = 0
+  const secondsIn1Day = 24 * 60 * 60
+  if (unit === 'Days') {
+    durationSec = amount * secondsIn1Day
+  }
+  if (unit === 'Weeks') {
+    durationSec = amount * secondsIn1Day * 7
+  }
+  if (unit === 'Months') {
+    durationSec = amount * secondsIn1Day * 7 * 4
+  }
+  if (unit === 'Years') {
+    durationSec = amount * secondsIn1Day * 7 * 4 * 365
+  }
+
+  return durationSec.toString()
+}
