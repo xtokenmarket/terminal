50c905f710d4ce58e0b5f2e9202e4c93e7fbc323
Eswara Sai <eswarasai.v@gmail.com>
2022-04-18 18:29:31 +0530

    Added Origination contracts

--- src/abis/index.ts
+++ src/abis/index.ts
@@ -8,6 +8,10 @@ import UniswapV3PoolStateAbi from './UniswapV3PoolState.json'
 import UniswapV3PositionAbi from './UniswapV3Position.json'
 import xAssetCLRAbi from './xAssetCLR.json'
 
+// Origination
+import OriginationCoreAbi from './origination/OriginationCore.json'
+import OriginationPoolAbi from './origination/OriginationPool.json'
+
 const Abi = {
   ERC20: ERC20Abi,
   LMTerminal: LMTerminalAbi,
@@ -18,6 +22,10 @@ const Abi = {
   UniswapV3PoolState: UniswapV3PoolStateAbi,
   UniswapV3Position: UniswapV3PositionAbi, // NonfungiblePositionManager
   xAssetCLR: xAssetCLRAbi,
+
+  // Origination
+  OriginationCore: OriginationCoreAbi,
+  OriginationPool: OriginationPoolAbi,
 }
 
 export default Abi
