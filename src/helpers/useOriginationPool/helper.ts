ffe6bd3a242102b20ea7e006ddf2864c11acc421
Nora Liu <noraliu0830@gmail.com>
2022-04-25 17:51:21 -0400

    fix: fix parsing input value logic

--- src/helpers/useOriginationPool/helper.ts
+++ src/helpers/useOriginationPool/helper.ts
@@ -1,7 +1,6 @@
 import Abi from 'abis'
 import { BigNumber } from 'ethers'
 import { MulticallService } from 'services'
-
 interface ITokenOfferDetails {
   offerToken: string
   purchaseToken: string
@@ -9,118 +8,16 @@ interface ITokenOfferDetails {
   offerTokenAmountSold: BigNumber
   startingPrice: BigNumber
   endingPrice: BigNumber
+  saleDuration: BigNumber
+  saleInitiatedTimestamp: BigNumber
   saleEndTimestamp: BigNumber
   vestingPeriod: BigNumber
   cliffPeriod: BigNumber
 }
 
 export const getOffersDataMulticall = async (
-<<<<<<< HEAD
-<<<<<<< HEAD
-    tokenOfferAddress: string,
-    multicall: MulticallService
-  ) => {
-    try {
-      // console.time(`loadInfo multicall ${tokenOfferAddress}`)
-      const calls = [
-        'totalOfferingAmount',
-        'offerTokenAmountSold',
-        'startingPrice',
-        'endingPrice',
-        'saleDuration',
-        'saleEndTimestamp',
-        'saleInitiatedTimestamp',
-        'vestingPeriod ',
-        'cliffPeriod',
-        'offerToken',
-        'purchaseToken'
-      ].map((method) => ({
-        name: method,
-        address: tokenOfferAddress,
-        params: [],
-      }))
-      const [
-        [totalOfferingAmount],
-        [offerTokenAmountSold],
-        [startingPrice],
-        [endingPrice],
-        [saleDuration],
-        [saleEndTimestamp],
-        [saleInitiatedTimestamp],
-        [vestingPeriod ],
-        [cliffPeriod],
-        [offerToken],
-        [purchaseToken]
-      ] = await multicall.multicallv2(Abi.OriginationPool, calls, {
-        requireSuccess: false,
-      })
-      return {
-        totalOfferingAmount,
-        offerTokenAmountSold,
-        startingPrice,
-        endingPrice,
-        saleDuration,
-        saleEndTimestamp,
-        saleInitiatedTimestamp,
-        vestingPeriod,
-        cliffPeriod,
-        offerToken,
-        purchaseToken
-      }
-    } catch (error) {
-      console.error('getOffersDataMulticall', error)
-=======
-  tokenOfferPoolAddress: string,
-=======
   poolAddress: string,
->>>>>>> d5228be (fix: rename tokenOfferPoolAddress to poolAddress for it to be commonly used on both Token Sale and NFT Mint)
   multicall: MulticallService
-<<<<<<< HEAD
-) => {
-  try {
-    const calls = [
-      'totalOfferingAmount',
-      'offerTokenAmountSold',
-      'startingPrice',
-      'endingPrice',
-      'saleDuration',
-      'saleEndTimestamp',
-      'saleInitiatedTimestamp',
-      'vestingPeriod ',
-      'cliffPeriod',
-    ].map((method) => ({
-      name: method,
-      address: tokenOfferPoolAddress,
-      params: [],
-    }))
-    const [
-      [totalOfferingAmount],
-      [offerTokenAmountSold],
-      [startingPrice],
-      [endingPrice],
-      [saleDuration],
-      [saleEndTimestamp],
-      [saleInitiatedTimestamp],
-      [vestingPeriod],
-      [cliffPeriod],
-    ] = await multicall.multicallv2(Abi.OriginationPool, calls, {
-      requireSuccess: false,
-    })
-    return {
-      totalOfferingAmount,
-      offerTokenAmountSold,
-      startingPrice,
-      endingPrice,
-      saleDuration,
-      saleEndTimestamp,
-      saleInitiatedTimestamp,
-      vestingPeriod,
-      cliffPeriod,
->>>>>>> 34f2682 (Bootstrapped token offer details page)
-    }
-  } catch (error) {
-    console.error(error)
-=======
 ): Promise<ITokenOfferDetails> => {
   const calls = [
     'offerToken',
@@ -129,6 +26,8 @@ export const getOffersDataMulticall = async (
     'offerTokenAmountSold',
     'startingPrice',
     'endingPrice',
+    'saleDuration',
+    'saleInitiatedTimestamp',
     'saleEndTimestamp',
     'vestingPeriod ',
     'cliffPeriod',
@@ -144,6 +43,8 @@ export const getOffersDataMulticall = async (
     [offerTokenAmountSold],
     [startingPrice],
     [endingPrice],
+    [saleDuration],
+    [saleInitiatedTimestamp],
     [saleEndTimestamp],
     [vestingPeriod],
     [cliffPeriod],
@@ -158,9 +59,10 @@ export const getOffersDataMulticall = async (
     offerTokenAmountSold,
     startingPrice,
     endingPrice,
+    saleDuration,
     saleEndTimestamp,
+    saleInitiatedTimestamp,
     vestingPeriod,
     cliffPeriod,
->>>>>>> 399d09b (Fixed discover page data wiring setup)
   }
 }
--- src/helpers/useOriginationPools/index.ts
+++ src/helpers/useOriginationPools/index.ts
@@ -1,17 +1,11 @@
 import axios from 'axios'
-import { TERMINAL_API_URL } from 'config/constants'
 import { getContractAddress } from 'config/networks'
 import { useConnectedWeb3Context } from 'contexts'
 import { useNetworkContext } from 'contexts/networkContext'
-<<<<<<< HEAD
-import { BigNumber } from 'ethers'
-=======
 import { useServices } from 'helpers'
->>>>>>> 2db2c06 (fix: use originationService from useService)
 import { useEffect, useState } from 'react'
 import { OriginationService } from 'services'
 import { ITokenOffer } from 'types'
-import { Network } from 'utils/enums'
 import { isTestnet, isTestNetwork } from 'utils/network'
 
 interface IState {
@@ -20,56 +14,7 @@ interface IState {
   tokenOffers: string[]
 }
 
-<<<<<<< HEAD:src/helpers/useTokenOffers/index.ts
-const offerings = [
-  {
-    network: Network.KOVAN,
-    poolAddress: '0x21bf5edacb55697b6f1352e453a42b0f6c2cf87e',
-    offerToken: {
-      address: '0x016750ac630f711882812f24dba6c95b9d35856d',
-      decimals: 6,
-      image: '/assets/tokens/usdt.png',
-      name: 'Tether USD',
-      symbol: 'USDT',
-    },
-    purchaseToken: {
-      address: '0x90410304D88E333710703aF6Ed6A14d5ef74575F',
-      decimals: 18,
-      image: '/assets/tokens/dai.png',
-      name: 'DAI',
-      symbol: 'DAI',
-    },
-    totalOfferingAmount: BigNumber.from('1500000000000'),
-<<<<<<< HEAD
-    remainingOfferingAmount: BigNumber.from('497303000000'),
-    pricePerToken: BigNumber.from('1250000000000000000'),
-    timeRemaining: BigNumber.from('23420'),
-    vestingPeriod: 31622400,
-    cliffPeriod: 7890000,
-=======
-    offerTokenAmountSold: BigNumber.from('1002697000000'),
-    startingPrice: BigNumber.from('1250000000000000000'),
-    endingPrice: BigNumber.from('1250000000000000000'),
-    saleEndTimestamp: BigNumber.from('1653141257'),
-    vestingPeriod: BigNumber.from('31622400'),
-    cliffPeriod: BigNumber.from('7890000'),
->>>>>>> 399d09b (Fixed discover page data wiring setup)
-  },
-]
-
-const getTokenOffers = async (): Promise<{ data: ITokenOffer[] }> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ data: offerings })
-    }, 2 * 1000)
-  })
-  // await axios.get<ITerminalPool[]>(`${TERMINAL_API_URL}/token-offers`)
-}
-
-export const useTokenOffers = () => {
-=======
 export const useOriginationPools = () => {
->>>>>>> 68a7ccc (refactor: rename useTokenOffer to useOriginationPool for keeping it generic between both type of sales):src/helpers/useOriginationPools/index.ts
   const [state, setState] = useState<IState>({
     tokenOffers: [],
     isLoading: true,
@@ -94,26 +39,13 @@ export const useOriginationPools = () => {
 
       // TODO: offers data pull from the contract, can be deleted after api is ready
       const readonlyProvider = provider
-<<<<<<< HEAD
-      const originationAddress = getContractAddress(
-        'origination',
-        readonlyProvider?.network.chainId
-      )
-      const origination = new OriginationService(
-        provider,
-        account,
-        originationAddress
-      )
-=======
 
->>>>>>> 2db2c06 (fix: use originationService from useService)
       const createFungibleListingFilter =
         originationService.contract.filters.CreateFungibleListing()
       const tokenOffers = await originationService.contract.queryFilter(
         createFungibleListingFilter
       )
       const tokenOfferAddresses = tokenOffers.map((offer) => offer.args?.pool)
-
       setState((prev) => ({
         ...prev,
         tokenOffers: tokenOfferAddresses,
