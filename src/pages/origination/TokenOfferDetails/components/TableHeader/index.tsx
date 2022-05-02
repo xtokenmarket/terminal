81b3232a6686cdbcc343c7474024a468134a7283
Nora <noraliu0830@gmail.com>
2022-05-02 09:46:25 -0400

    feat: add origination detail page UI (#127)
    * feat: add origination detail page UI
    
    * feat: add whiltelist
    
    * feat: add public sale
    
    * fix: type
    
    * feat: add my position
    
    * feat: add invest / set whitelist / initiate sale button
    
    * fix: typo

---
+++ src/pages/origination/TokenOfferDetails/components/TableHeader/index.tsx
@@ -0,0 +1,141 @@
+import clsx from 'clsx'
+import { makeStyles } from '@material-ui/core'
+import { Td } from '../Td'
+import {
+  MyPosition,
+  OfferingOverview,
+  OriginationLabels,
+  PublicSale,
+  WhitelistSale,
+} from 'utils/enums'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    marginBottom: 12,
+  },
+  item: {
+    color: theme.colors.purple0,
+    fontSize: 12,
+    display: 'flex',
+    alignItems: 'center',
+  },
+  itemAlignRight: {
+    justifyContent: 'flex-end',
+  },
+}))
+
+interface IProps {
+  label: string
+}
+
+export const TableHeader = (props: IProps) => {
+  const classes = useStyles()
+
+  if (props.label === OriginationLabels.OfferingOverview) {
+    return (
+      <div className={classes.root}>
+        <Td type={OfferingOverview.OfferToken} label={props.label}>
+          <div className={classes.item}>Offer Token</div>
+        </Td>
+        <Td type={OfferingOverview.PurchaseToken} label={props.label}>
+          <div className={classes.item}>Purchase Token</div>
+        </Td>
+        <Td type={OfferingOverview.OfferingStatus} label={props.label}>
+          <div className={classes.item}>Offering Status</div>
+        </Td>
+        <Td type={OfferingOverview.OfferingReserve} label={props.label}>
+          <div className={classes.item}>Offering Reserve</div>
+        </Td>
+        <Td type={OfferingOverview.VestingPeriod} label={props.label}>
+          <div className={classes.item}>Vesting Period</div>
+        </Td>
+        <Td type={OfferingOverview.CliffPeriod} label={props.label}>
+          <div className={classes.item}>Cliff Period</div>
+        </Td>
+        <Td type={OfferingOverview.SalesBegin} label={props.label}>
+          <div className={classes.item}>Sales Begin</div>
+        </Td>
+        <Td type={OfferingOverview.SalesEnd} label={props.label}>
+          <div className={classes.item}>Sales End</div>
+        </Td>
+        <Td type={OfferingOverview.SalesPeriod} label={props.label}>
+          <div className={clsx(classes.item, classes.itemAlignRight)}>
+            Sales Period
+          </div>
+        </Td>
+      </div>
+    )
+  }
+
+  if (props.label === OriginationLabels.WhitelistSale) {
+    return (
+      <div className={classes.root}>
+        <Td type={WhitelistSale.CurrentPrice} label={props.label}>
+          <div className={classes.item}>Current Price</div>
+        </Td>
+        <Td type={WhitelistSale.PricingFormular} label={props.label}>
+          <div className={classes.item}>Pricing Formular</div>
+        </Td>
+        <Td type={WhitelistSale.StartingEndingPrice} label={props.label}>
+          <div className={classes.item}>Starting / Ending Price</div>
+        </Td>
+        <Td type={WhitelistSale.Whitelist} label={props.label}>
+          <div className={classes.item}>Whitelist</div>
+        </Td>
+        <Td type={WhitelistSale.AddressCap} label={props.label}>
+          <div className={classes.item}>Address Cap</div>
+        </Td>
+        <Td type={WhitelistSale.TimeRemaining} label={props.label}>
+          <div className={classes.item}>Time Remaining</div>
+        </Td>
+        <Td type={WhitelistSale.SalesPeriod} label={props.label}>
+          <div className={classes.item}>Sales Period</div>
+        </Td>
+      </div>
+    )
+  }
+
+  if (props.label === OriginationLabels.PublicSale) {
+    return (
+      <div className={classes.root}>
+        <Td type={PublicSale.CurrentPrice} label={props.label}>
+          <div className={classes.item}>Current Price</div>
+        </Td>
+        <Td type={PublicSale.PricingFormular} label={props.label}>
+          <div className={classes.item}>Pricing Formular</div>
+        </Td>
+        <Td type={PublicSale.Price} label={props.label}>
+          <div className={classes.item}>Price</div>
+        </Td>
+        <Td type={PublicSale.TimeRemaining} label={props.label}>
+          <div className={classes.item}>Time Remaining</div>
+        </Td>
+        <Td type={PublicSale.SalesPeriod} label={props.label}>
+          <div className={classes.item}>Sales Period</div>
+        </Td>
+      </div>
+    )
+  }
+
+  if (props.label === OriginationLabels.MyPosition) {
+    return (
+      <div className={classes.root}>
+        <Td type={MyPosition.TokenPurchased} label={props.label}>
+          <div className={classes.item}>Token Purchased</div>
+        </Td>
+        <Td type={MyPosition.AmountInvested} label={props.label}>
+          <div className={classes.item}>Amount Invested</div>
+        </Td>
+        <Td type={MyPosition.Amountvested} label={props.label}>
+          <div className={classes.item}>Amount Vested</div>
+        </Td>
+        <Td type={MyPosition.AmountAvailableToVest} label={props.label}>
+          <div className={classes.item}>Amount Available To Vest</div>
+        </Td>
+      </div>
+    )
+  }
+
+  return <></>
+}
