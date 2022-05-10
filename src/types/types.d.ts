918308a24430bf4c59c52e618114aa7c40374253
Nora Liu <noraliu0830@gmail.com>
2022-05-10 19:00:44 +0800

    feat: wire up whitelist data

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -201,6 +201,7 @@ export interface ITokenOffer {
   poolAddress?: string
   offeringOverview: IOfferingOverview
   originationRow: IOriginationRow
+  whitelist: IWhitelistSale
 }
 
 interface Label {
@@ -216,24 +217,26 @@ export interface IOfferingOverview extends Label {
   purchaseToken: IToken
   salesBegin: BigNumber
   salesEnd: BigNumber
-  salesPeriod: BigNumber
+  salesPeriod?: BigNumber
   totalOfferingAmount: BigNumber
   vestingPeriod: BigNumber
 }
 
 export interface IOriginationRow extends IOfferingOverview {
   startingPrice: BigNumber
-  saleDuration: BigNumber
+  saleDuration?: BigNumber
 }
 
 export interface IWhitelistSale extends Label {
   currentPrice: string
   pricingFormular: string
-  startingEndingPrice: string
-  whitelist: string
+  startingPrice?: BigNumber
+  endingPrice?: BigNumber
+  whitelist?: string[]
   addressCap: string
   timeRemaining: string
-  salesPeriod: string
+  salesPeriod?: BigNumber
+  offerToken: IToken
 }
 
 export interface IPublicSale extends Label {
