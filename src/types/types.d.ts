24bf5657df4d28a67924d7136bca0c2df723fa14
Nora Liu <noraliu0830@gmail.com>
2022-05-11 09:50:11 +0800

    feat: add public sale data

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -253,6 +253,7 @@ export interface ITokenOffer {
   offeringOverview: IOfferingOverview
   originationRow: IOriginationRow
   whitelist: IWhitelistSale
+  publicSale: IPublicSale
 }
 
 interface Label {
@@ -294,8 +295,8 @@ export interface IPublicSale extends Label {
   currentPrice: string
   pricingFormular: string
   price: string
-  timeRemaining: string
-  salesPeriod: string
+  salesPeriod?: BigNumber
+  saleEndTimestamp: BigNumber
 }
 
 export interface IMyPosition extends Label {
